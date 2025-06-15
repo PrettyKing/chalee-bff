978f480bcea71ad5782631ac2c614bbf60602cfe
chalee <695469701@qq.com>
2025-06-15 23:00:47 +0800

    feat: U

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 import devConfig from './dev';
 import prodConfig from './prod';
+import { join } from 'path';
 
 let config = {
   port: 8081,
   memoryFlag: false,
+  viewDir: join(__dirname, '../../../dist/web/'),
+  staticDir: join(__dirname, '../../../dist/web/'),
+
 };
 const env = process.env.NODE_ENV || 'development';
 if (env === 'production') {
