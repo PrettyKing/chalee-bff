0c22e090ec8661d843e02c11a0256f7c434298c8
chalee <695469701@qq.com>
2025-06-15 20:41:46 +0800

    feat: u

--- src/server/config/prod.ts
+++ src/server/config/prod.ts
@@ -3,6 +3,6 @@ import { join } from "path";
 export default {
     port: 8082,
     memoryFlag: 'memory',
-    viewDir: join(__dirname, '/web/'),
-    staticDir: join(__dirname, '/web/'),
+    viewDir: join(__dirname, '../web/'),
+    staticDir: join(__dirname, '../web/'),
 };
