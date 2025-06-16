628eb76cc961b34336415ffd57b8c87c7a477d52
chalee <695469701@qq.com>
2025-06-16 14:58:23 +0800

    feat: test

--- src/web/pages/Home.tsx
+++ src/web/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mt-5">
-      <h1 className="text-4xl font-bold mb-4">欢迎来到我们的 BFF</h1>
+      <h1 className="text-4xl font-bold mb-4">欢迎来到我们的 BFF JENKINS TEST</h1>
       <p className="text-lg text-gray-700">这是一个简单的去中心化应用示例。</p>
     </div>
   );
